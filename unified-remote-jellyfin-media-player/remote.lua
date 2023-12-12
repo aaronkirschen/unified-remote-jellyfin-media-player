80b82143641fa6ddfc56e70ea482662b681abd62
Aaron Kirschen <aaronkirschen@gmail.com>
2023-12-12 12:53:52 -0800

    Init

---
+++ unified-remote-jellyfin-media-player/remote.lua
@@ -0,0 +1,24 @@
+--@help Launch Jellyfin Media Player
+actions.launch = function()
+  -- Launch code
+end
+
+--@help Decrease Jellyfin playback volume
+actions.volumeDown = function()
+  -- Volume down code
+end
+
+--@help Increase Jellyfin playback volume
+actions.volumeUp = function()
+  -- Volume up code
+end
+
+--@help Pause Jellyfin playback
+actions.pause = function()
+  -- Pause code
+end
+
+--@help Resume Jellyfin playback
+actions.resume = function()
+  -- Resume code
+end
